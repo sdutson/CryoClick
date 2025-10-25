a4b382524757e62ef11f65d2f34e4b3a4dcad1fb
Eva Giddings <dubstep9930@gmail.com>
2025-10-24 18:56:15 -0600

    fixedbackground

--- src/scenes/Start.js
+++ src/scenes/Start.js
@@ -5,7 +5,7 @@ export class Start extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('background', 'assets/mars_background.webp');
+        this.load.image('background', 'assets/mars_background.png');
         this.load.image('logo', 'assets/phaser.png');
 
         //  The ship sprite is CC0 from https://ansimuz.itch.io - check out his other work!
@@ -13,7 +13,7 @@ export class Start extends Phaser.Scene {
     }
 
     create() {
-        this.background = this.add.tileSprite(640, 360, 1800, 1200, 'background');
+        this.background = this.add.tileSprite(640, 360, 1280, 719, 'background');
 
         //const logo = this.add.image(640, 200, 'logo');
 
