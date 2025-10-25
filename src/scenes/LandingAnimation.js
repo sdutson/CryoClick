6064989df1bf0afbeb4139872a3e63f0257c2514
sdutson <sdutson222@gmail.com>
2025-10-24 20:25:59 -0600

    added context message

--- src/scenes/LandingAnimation.js
+++ src/scenes/LandingAnimation.js
@@ -9,13 +9,43 @@ export class LandingAnimation extends Phaser.Scene {
 
         this.load.image('explosion', 'assets/explosion_scene_2.png');
 
-        //  The ship sprite is CC0 from https://ansimuz.itch.io - check out his other work!
+        //  The ship sprite is CC0 from https://ansimuz.itch.io/ - check out his other work!
         this.load.spritesheet('ship', 'assets/spaceship.png', { frameWidth: 176, frameHeight: 96 });
     }
 
     create() {
+        // Terminal text first
+        const terminalText = this.add.text(50, 50, '', {
+            fontFamily: 'monospace',
+            fontSize: '32px',
+            color: '#00ff00',
+        });
+
+        const message = "Initializing Mars Landing Protocol...\nChecking systems...\nPrepare for descent!";
+        let charIndex = 0;
+
+        // Type each character every 50ms
+        const typingEvent = this.time.addEvent({
+            delay: 50,
+            callback: () => {
+                terminalText.text += message[charIndex];
+                charIndex++;
+                if (charIndex >= message.length) {
+                    typingEvent.remove(); // stop typing
+
+                    // Once done, show background and ship
+                    this.showLanding();
+                }
+            },
+            loop: true
+        });
+    }
+
+    showLanding() {
+        // Background
         this.background = this.add.tileSprite(640, 360, 1280, 719, 'background');
 
+        // Ship
         const ship = this.add.sprite(640, 360, 'ship');
         ship.rotation = 45;
 
@@ -30,33 +60,47 @@ export class LandingAnimation extends Phaser.Scene {
 
         this.tweens.add({
             targets: ship,
-            x: ship.x + 200,  // move forward while falling
-            y: 600,           // ground level
+            x: ship.x + 200,
+            y: 600,
             duration: 2000,
             ease: 'Cubic.easeIn',
             onComplete: () => {
                 ship.setVisible(false);
 
-                // Optional explosion PNG
                 const explosion = this.add.image(ship.x, ship.y, 'explosion');
                 explosion.setOrigin(0.5, 1);
                 explosion.setScale(0.5);
 
-                // Add text in upper-right corner
-                const textX = this.cameras.main.width - 20; // 20px padding from right
-                const textY = 20;                           // 20px padding from top
-                const infoText = this.add.text(textX, textY, 'Quickly escape to the hab!!!', {
-                    fontSize: '45px',
-                    color: 'white'
-                }).setOrigin(1, 0); // align top-right
+                // Terminal text first
+                const terminalText = this.add.text(50, 50, '', {
+                    fontFamily: 'monospace',
+                    fontSize: '32px',
+                    color: '#ffffff',
+                });
 
-                // After 5 seconds, move to next scene
-                this.time.delayedCall(5000, () => {
-                    this.scene.start('BeforeCryo'); // next scene
+                const message = "Quick! Get to the hab!";
+                let charIndex = 0;
+
+                // Type each character every 50ms
+                const typingEvent = this.time.addEvent({
+                    delay: 50,
+                    callback: () => {
+                        terminalText.text += message[charIndex];
+                        charIndex++;
+                        if (charIndex >= message.length) {
+                            typingEvent.remove(); // stop typing
+
+                            // Pause 3 seconds before moving to next scene
+                            this.time.delayedCall(3000, () => {
+                                this.scene.start('BeforeCryo');
+                            });
+                        }
+                    },
+                    loop: true
                 });
             }
         });
-
     }
+
     
 }
