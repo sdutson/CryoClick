b4a35a1a91f4bfefb62a51d894f943cf38c045bb
Will Jackson <williamjackson2020@gmail.com>
2025-10-24 20:52:44 -0600

    ship from corner

--- src/scenes/LandingAnimation.js
+++ src/scenes/LandingAnimation.js
@@ -42,65 +42,69 @@ export class LandingAnimation extends Phaser.Scene {
     }
 
     showLanding() {
-        // Background
-        this.background = this.add.tileSprite(640, 360, 1280, 719, 'background');
+    // Background
+    this.background = this.add.tileSprite(640, 360, 1280, 720, 'background');
 
-        // Ship
-        const ship = this.add.sprite(640, 360, 'ship');
-        ship.rotation = 45;
+    // Ship starts offscreen top-left
+    const ship = this.add.sprite(-100, -100, 'ship');
+    ship.rotation = 0.8; // slight diagonal angle
 
-        ship.anims.create({
-            key: 'fly',
-            frames: this.anims.generateFrameNumbers('ship', { start: 0, end: 2 }),
-            frameRate: 15,
-            repeat: -1
-        });
+    // Animate the ship
+    ship.anims.create({
+        key: 'fly',
+        frames: this.anims.generateFrameNumbers('ship', { start: 0, end: 2 }),
+        frameRate: 15,
+        repeat: -1
+    });
+    ship.play('fly');
 
-        ship.play('fly');
+    // Tween to landing spot (fast, 2 seconds)
+    this.tweens.add({
+        targets: ship,
+        x: 640,   // end X position (center-ish)
+        y: 600,   // end Y position (bottom area)
+        duration: 2000,      // fast speed
+        ease: 'Cubic.easeIn',
+        onComplete: () => {
+            // Explosion exactly at ship’s final position
+            const explosion = this.add.image(ship.x, ship.y, 'explosion');
+            explosion.setOrigin(0.5, 1);
+            explosion.setScale(0.5);
 
-        this.tweens.add({
-            targets: ship,
-            x: ship.x + 200,
-            y: 600,
-            duration: 2000,
-            ease: 'Cubic.easeIn',
-            onComplete: () => {
-                ship.setVisible(false);
+            // Hide ship
+            ship.setVisible(false);
 
-                const explosion = this.add.image(ship.x, ship.y, 'explosion');
-                explosion.setOrigin(0.5, 1);
-                explosion.setScale(0.5);
+            // Show terminal message
+            const terminalText = this.add.text(50, 50, '', {
+                fontFamily: 'monospace',
+                fontSize: '32px',
+                color: '#ffffff',
+            });
 
-                // Terminal text first
-                const terminalText = this.add.text(50, 50, '', {
-                    fontFamily: 'monospace',
-                    fontSize: '32px',
-                    color: '#ffffff',
-                });
+            const message = "Quick! Get to the hab!";
+            let charIndex = 0;
 
-                const message = "Quick! Get to the hab!";
-                let charIndex = 0;
+            const typingEvent = this.time.addEvent({
+                delay: 50,
+                callback: () => {
+                    terminalText.text += message[charIndex];
+                    charIndex++;
+                    if (charIndex >= message.length) {
+                        typingEvent.remove();
 
-                // Type each character every 50ms
-                const typingEvent = this.time.addEvent({
-                    delay: 50,
-                    callback: () => {
-                        terminalText.text += message[charIndex];
-                        charIndex++;
-                        if (charIndex >= message.length) {
-                            typingEvent.remove(); // stop typing
+                        // Wait 3 seconds, then move to next scene
+                        this.time.delayedCall(3000, () => {
+                            this.scene.start('BeforeCryo');
+                        });
+                    }
+                },
+                loop: true
+            });
+        }
+    });
+}
 
-                            // Pause 3 seconds before moving to next scene
-                            this.time.delayedCall(3000, () => {
-                                this.scene.start('BeforeCryo');
-                            });
-                        }
-                    },
-                    loop: true
-                });
-            }
-        });
-    }
+
 
     
 }
